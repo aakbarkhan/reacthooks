9af6c9a8661c7779354a5725dcd1b589075c26b7
aakbarkhan <aakuukhan@gmail.com>
2022-08-12 01:11:54 +0530

    add usecallback

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import './App.css';
 
 // this section is for suing callBack hooks in react.useCallback, useState, useEffect,
 
-// function App() {
-//   const [userInput, setUserInput ] = useState('');
-//   const [result, setResult ] = useState(0);
+function App() {
+  const [userInput, setUserInput ] = useState('');
+  const [result, setResult ] = useState(0);
 
-//   const [num1] = useState(4);
-//   const [num2] = useState(5);
+  const [num1] = useState(4);
+  const [num2] = useState(5);
 
-//   //const sum = useCallback(() => num1 + num2, [num1, num2]);
-//   const buildArray = useCallback(() => [num1, num2], [num1, num2]);
+const sum = useCallback(() => num1 + num2, [num1, num2]);
+  const buildArray = useCallback(() => [num1, num2], [num1, num2]);
 
-//   useEffect(() => {
-//       console.log(`New array: ${buildArray()}`);
-//       setResult(buildArray());
-//   }, [buildArray]);
-//   return (
-//     <main className='App'>
-//       <input type='text' placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-//       <h1>Output: {userInput || "--"}</h1>
-//       <p>Result: {JSON.stringify(result)}</p>
-//     </main>
-//   );
-// }
+  useEffect(() => {
+      console.log(`New array: ${buildArray()}`);
+      setResult(buildArray());
+  }, [buildArray]);
+  return (
+    <main className='App'>
+      <input type='text' placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+      <h1>Output: {userInput || "--"}</h1>
+      <p>Result: {JSON.stringify(result)}</p>
+    </main>
+  );
+}
 
 // Using the useMemo hooks. useCallback, useState, useEffect, useMemo
 
@@ -65,59 +65,59 @@ import './App.css';
 //   );
 // }
 
-function App() {
-  const [randomInput, setRandomInput] = useState('');
-  const [seconds, setSeconds ] = useState(0);
-  const renders = useRef(0);
-  // const inputRef = useRef();
-  const timerId = useRef();
+// function App() {
+//   const [randomInput, setRandomInput] = useState('');
+//   const [seconds, setSeconds ] = useState(0);
+//   const renders = useRef(0);
+//   // const inputRef = useRef();
+//   const timerId = useRef();
 
-  const handleChange = (e) => {
-    setRandomInput(e.target.value)
-    renders.current++;
-  }
+//   const handleChange = (e) => {
+//     setRandomInput(e.target.value)
+//     renders.current++;
+//   }
 
   // const focusInput = () => {
   //   inputRef.current.focus();
   // }
 
-  const startTimer = () => {
-    timerId.current = setInterval(() => {
-      setSeconds( prev => prev + 1)
-      renders.current++;
-    }, 1000)
-    console.log()
-  }
+  // const startTimer = () => {
+  //   timerId.current = setInterval(() => {
+  //     setSeconds( prev => prev + 1)
+  //     renders.current++;
+  //   }, 1000)
+  //   console.log()
+  // }
 
-  const stopTimer = () => {
-    clearInterval(timerId.current);
-    timerId.current = 0;
-    console.log('stop timer');
-  }
+  // const stopTimer = () => {
+  //   clearInterval(timerId.current);
+  //   timerId.current = 0;
+  //   console.log('stop timer');
+  // }
 
-  const resetTimer = () => {
-    stopTimer();
-    if(seconds) {
-      renders.current++;
-      setSeconds(0);
+  // const resetTimer = () => {
+  //   stopTimer();
+  //   if(seconds) {
+  //     renders.current++;
+  //     setSeconds(0);
 
-    }
-  }
+  //   }
+  // }
 
-  return (
-    <main className='App'>
-      <input type='text' value={randomInput} placeholder='Random Input' onChange={handleChange} />
-      <br></br>
-      <p>Randers: {renders.current}</p>
-      <br></br>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-      <button onClick={resetTimer}>Reset</button>
-      <br></br>
-      <p>OutputRender:{randomInput}</p>
-      <p>Seconds:{seconds}</p>
-    </main>
-  )
-}
+//   return (
+//     <main className='App'>
+//       <input type='text' value={randomInput} placeholder='Random Input' onChange={handleChange} />
+//       <br></br>
+//       <p>Randers: {renders.current}</p>
+//       <br></br>
+//       <button onClick={startTimer}>Start</button>
+//       <button onClick={stopTimer}>Stop</button>
+//       <button onClick={resetTimer}>Reset</button>
+//       <br></br>
+//       <p>OutputRender:{randomInput}</p>
+//       <p>Seconds:{seconds}</p>
+//     </main>
+//   )
+// }
 
 export default App;
